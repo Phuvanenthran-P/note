b8bdf5f3de838c51f25604d441de941525bcce74
Phuvanenthran-P <158295023+Phuvanenthran-P@users.noreply.github.com>
2025-10-03 11:49:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-self.addEventListener("install", e=>self.skipWaiting());
-self.addEventListener("activate", e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("install", e => {
+  console.log("Service Worker installed");
+});
 
-self.addEventListener("notificationclick", e=>{
-  if(e.action==="remove"){
-    e.notification.close();
-    e.waitUntil(self.clients.matchAll({type:"window"}).then(clients=>{
-      if(clients.length){clients[0].postMessage({type:"removeNote",id:e.notification.data.id});}
-    }));
-  }else{
-    e.notification.close();
-    e.waitUntil(self.clients.openWindow("/"));
-  }
+self.addEventListener("activate", e => {
+  console.log("Service Worker activated");
 });
